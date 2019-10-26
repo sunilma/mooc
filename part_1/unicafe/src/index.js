1efb8942cefd5ae8c0b2c777a717fb0499dea7e2
Sunil Maharjan <ratnasunil70@gmail.com>
2019-10-26 16:11:08 +0300

    1.11 unicafe step 6 completed

--- part_1/unicafe/src/index.js
+++ part_1/unicafe/src/index.js
@@ -1,24 +1,37 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { addComment } from "@babel/types";
+
+const Statistics = ({ text, value }) => (
+  <p>
+    {text} {value}
+  </p>
+);
+
+const Button = ({ name, val, setVal }) => (
+  <button onClick={() => setVal(val + 1)}>{name}</button>
+);
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Button name="Good" val={good} setVal={setGood} />
+      <Button name="Neutral" val={neutral} setVal={setNeutral} />
+      <Button name="Bad" val={bad} setVal={setBad} />
       <br />
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
+      <Statistics text="Good" value={good} />
+      <Statistics text="Neutral" value={neutral} />
+      <Statistics text="Bad" value={bad} />
+      <Statistics text="All" value={total} />
+      <Statistics text="Average" value={total ? (good - bad) / total : 0} />
+      <Statistics text="Positive" value={total ? (good * 100) / total : 0} />
     </div>
   );
 };
